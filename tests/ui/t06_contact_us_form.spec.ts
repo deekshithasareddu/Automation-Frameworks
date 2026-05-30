af09f210e86506157f32b64a81228224cef20a2f
Naga Deekshitha <deekshithasareddu@gmail.com>
2026-05-30 12:51:26 -0400

    Test Case 6 - Contact Us Form

--- tests/ui/t06_contact_us_form.spec.ts
+++ tests/ui/t06_contact_us_form.spec.ts
@@ -1,28 +1,51 @@
 import { test, expect } from '@playwright/test';
-import { TestData } from '../../fixtures/TestData.page';
-  import path from 'path';
 
 test('Contact Us Form Test', async ({ page }) => {
 
-  // 1. Launch browser & 2. Navigate to URL
+  // 1. Launch browser & Navigate to URL
   await page.goto('https://automationexercise.com');
 
-  // 3. Verify home page is visible successfully
+  // 2. Verify home page is visible successfully
   await expect(page).toHaveTitle(/Automation Exercise/);
-   // 4. Click on 'Contact Us' button
-  await page.click('a[href="/contact_us"]');
 
-  // 5. Verify 'GET IN TOUCH' is visible
+  // 3. Click on Contact Us button
+  await page.locator('a[href="/contact_us"]').click();
+
+  // 4. Verify GET IN TOUCH is visible
   await expect(
     page.getByText('Get In Touch')
   ).toBeVisible();
-   // 6. Enter name, email, subject and message
-  await page.fill('[data-qa="name"]', 'Naga Deekshitha');
-  await page.fill('[data-qa="email"]', 'deekshitha@test.com');
-  await page.fill('[data-qa="subject"]', 'Playwright Testing');
-  await page.fill('[data-qa="message"]', 'This is a test message using Playwright.');
-  //upload file
-  await page.setInputFiles(
-  'input[name="upload_file"]',
-  path.join(__dirname, '../../test-data/sample.txt')
-);
+
+  // 5. Enter name, email, subject and message
+  await page.locator('[data-qa="name"]').fill('Naga Deekshitha');
+
+  await page.locator('[data-qa="email"]').fill('deekshitha@test.com');
+
+  await page.locator('[data-qa="subject"]').fill('Playwright Testing');
+
+  await page.locator('[data-qa="message"]').fill(
+    'This is a test message using Playwright.'
+  );
+
+  // 6. Accept alert popup
+  page.once('dialog', async dialog => {
+    await dialog.accept();
+  });
+
+  // 7. Click Submit button
+  await page.locator('[data-qa="submit-button"]').click();
+
+  // 8. Wait for success message
+  await expect(
+    page.locator('.status.alert.alert-success')
+  ).toContainText('Success', {
+    timeout: 10000
+  });
+
+  // 9. Navigate back to Home Page
+  await page.goto('https://automationexercise.com');
+
+  // 10. Verify Home Page loaded
+  await expect(page).toHaveTitle(/Automation Exercise/);
+
+});
